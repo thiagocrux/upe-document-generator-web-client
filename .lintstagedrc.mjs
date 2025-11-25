cf886989d03c80f873cfe326318563a099c24299
Thiago Cruz <thiagocruz.eu@gmail.com>
2025-11-25 00:14:01 -0300

    chore: config commit linters

---
+++ .lintstagedrc.mjs
@@ -0,0 +1,11 @@
+const lintStagedConfig = {
+  '*.{js,ts,mjs,mts}': (filenames) => [
+    `npx prettier --write ${filenames
+      .map((filename) => `"${filename}"`)
+      .join(' ')}`,
+    'eslint . --fix --ignore-pattern "dist/*"',
+    // config testing routine...,
+  ],
+};
+
+export default lintStagedConfig;
